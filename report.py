888a5264ff47ae0cb05d390052f42f02a87cc17d
DharshiniGenai <dharshiniai2306@gmail.com>
2026-05-23 12:08:18 +0530

    Delete report.py

--- report.py
+++
@@ -1,7 +0,0 @@
-def generate_summary(cleaned_data):
-
-    with open("outputs/summary_report.txt", "w") as f:
-
-        f.write("DATA CLEANING SUMMARY\n")
-
-        f.write(f"Total Clean Records: {len(cleaned_data)}\n")
